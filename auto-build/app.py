2d5f463c4feb57b8a99a6cf04e7eb88992fa8649
Rueskas <Rueskas@hotmail.com>
2020-03-31 15:42:18 +0200

    Construccion de Imagenes y Desarrollo de Contenedores

---
+++ auto-build/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from redis import Redis, RedisError
+import socket
+
+
+app = Flask(__name__)
+redis = Redis(host="redis")
+
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr('counter')
+    except RedisError:
+        visits = "<i>counter disabled. Cannot connect to Redis.</i>"
+
+    html = "<h2>Hola Mundo</h2>" \
+	   "<h3>Desde OpenWebinars</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}<br/>" \
+           "<br/>"
+
+    return html.format(hostname=socket.gethostname(), visits=visits)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
